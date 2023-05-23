3de45ad17ebd09f94f612d596de681fff5d964b2
BilelMH <bilel.mh1997@gmail.com>
2023-05-23 19:06:04 +0100

    fainelly

---
+++ myfirstapp/src/App.js
@@ -0,0 +1,130 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Stack from 'react-bootstrap/Stack';
+import Card from 'react-bootstrap/Card';
+import pic1 from './hotel.jpg' ;
+import pic2 from './hotel2.jpg';
+import pic3 from './hotel3.jpg';
+function App() {
+  return (
+  <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+      </Navbar> 
+      
+
+      <Stack direction="horizontal" gap={3}>
+      <Form.Control className="me-auto" placeholder="Add your item here..." />
+      <Button variant="secondary">Submit</Button>
+      <div className="vr" />
+      <Button variant="outline-danger">Reset</Button>
+      </Stack>
+    <br></br>
+    <br></br>
+    <br></br>
+      <Container id="carde">
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={ pic1 } />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card> 
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src= {pic2 } />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card> 
+
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={ pic3} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card> 
+        
+        
+        
+        
+        
+        
+        
+        </Container> 
+        <br></br>
+        <br></br>
+        <br></br>
+        <div className='formeo'>
+        <Form id="forme">
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+      
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+          </Form>
+          </div>
+ </div> 
+    ) ;
+  } 
+export default App ;
